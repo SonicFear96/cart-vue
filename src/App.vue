eb25d1c781ab08a1b279d1c70492ad13465ba2bb
Bogdan Rudakov <48532613+SonicFear96@users.noreply.github.com>
2023-06-25 14:17:46 +0300

    feature: scss, basic wrapper, component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,41 @@
 <template>
-  <div id="app">main page</div>
+  <div id="app">
+    <Header />
+    <div class="main-content wrapper">
+      <section class="menu-filter-wrapper">
+        <MenuFilter />
+      </section>
+      <section class="catalog-wrapper">
+        <ul class="catalog-list">
+          <li class="catalog-item">
+            <ProductCard />
+          </li>
+        </ul>
+      </section>
+    </div>
+    <Footer />
+  </div>
 </template>
 
 <script>
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+import MenuFilter from "@/components/menu-filter";
+import ProductCard from "@/components/product-card";
 export default {
   name: "App",
   components: {
-    //
+    Header,
+    Footer,
+    MenuFilter,
+    ProductCard,
   },
 };
 </script>
 
 <style lang="scss">
 #app {
-  //
+  max-width: 1600px;
+  margin: 0 auto;
 }
 </style>
---
+++ src/components/menu-filter/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="menu-filter-component">menu filter component</div>
+</template>
+
+<script>
+export default {
+  name: "menu-filter-component",
+};
+</script>
+
+<style lang="scss">
+.menu-filter-component {
+  //
+}
+</style>
