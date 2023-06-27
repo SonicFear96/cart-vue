ad2c3395554d6b61d76d1754ec8e8b6cd7b9c48e
Bogdan Rudakov <48532613+SonicFear96@users.noreply.github.com>
2023-06-27 22:57:47 +0300

    feature: deploy

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/cart-vue/" : "/",
   css: {
     loaderOptions: {
       scss: {
@@ -9,13 +10,3 @@ module.exports = defineConfig({
     },
   },
 });
-
-// module.exports = {
-//   css: {
-//     loaderOptions: {
-//       scss: {
-//         additionalData: `@import "~@/assets/scss/index.scss";`,
-//       },
-//     },
-//   },
-// };
